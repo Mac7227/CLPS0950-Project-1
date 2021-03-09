e471620229ffc5eb0bf36b2ff169f7bdcfa6fb9b
Mac7227 <79659030+Mac7227@users.noreply.github.com>
2021-03-09 13:25:50 -0500

    Add files via upload

---
+++ Project 1/Database_Final.m
@@ -0,0 +1,27 @@
+%% Below Coding Section by Mac and Maia
+
+function pictures = load_database();  %% creates function to load pictures into database
+
+persistent loaded; %% creates a persistent variable 'loaded'
+persistent numeric_image; %% creates a persistent variable 'numeric image'
+
+if(isempty(loaded)) %% if statement that executes following command if variable loaded is 0
+    all_Images = zeros(82369, 8); %% sets size of matrix that will be a string of the images with 82,369 pixels each (287 x 287)
+    cd('Pictures'); %% goes to Pictures folder where pictures are located
+    for ii = 1:4 %% for statement to run for the 4 individual folders
+        cd(strcat('P',num2str(ii))); %% opens individual folders P1 to P4 using steps from 1:4
+        for jj = 1:2 %% for statement to execute following command for both images per individual
+            image_contain = imread(strcat(num2str(jj), '.jpg')); %% opens images in each folder until 1.jpg and 2.jpg are open
+            all_images(:,(ii-1)*2+jj) = reshape(image_contain, size(image_contain,1)*size(image_contain,2),1); %% creates matrix of all images in numerical form stacked on top of each other
+        end %% ends for loop for each picture within the individual folders per person
+        disp('Pictures Loading'); %% Displays Pictures Loading for each set of Pictures
+        cd .. %% goes to previous folder which is 'Project 1'
+    end %% ends original for loop for each folder per person
+    numeric_image = uint8(all_images); %% converts the numeric image variable into an unsigned 8-bit integer version of all of the images
+end %% ends if statement loop
+loaded = 1; %% sets loaded variable equal to 1 
+pictures = numeric_image; %% sets output variable equal to the 
+end %% ends the function
+
+%% End of Coding by Mac and Maia -- Research, Coding, and DeBugging took 15 Hours
+%% This Time Includes -- Acquiring Pictures, Formatting and Editing Pictures, Arranging Folders of Pictures, and Uploading Pictures to be implemented into Database for Code
---
+++ Project 1/Face_Recognition_Final.m
@@ -0,0 +1,71 @@
+%% This is a game that allows the user to guess who the randomly indexed image is of
+%% then goes through the rest of the data set of pictures and finds the most similar image.
+%% After it finds the most similar image, it assesses that image and provides the correct answer
+%% of who the image is of.
+
+%% Below Coded by Mac -- Line 2 through Line 22
+
+loaded_image = Database_Final(); %% sets loaded image equal to the uploaded database
+
+
+random_index = round(7*rand(1)+1); %% creates variable that pulls random numbers from 1 to 8
+random_image = loaded_image(:,random_index); %% pulls associated image from set of images based on random index value
+rest_of_the_images = loaded_image(:,([1:random_index-1 random_index+1:end])); %% sets of rest images equal to the entire set of images except the random indexed image
+
+white_image = uint8(ones(1, size(rest_of_the_images,2))); %% creates an 8-bit integer string of ones for each of the rest of the images
+mean_value = uint8(mean(rest_of_the_images,2)); %% calculates the mean of the rest of the images using 8-bit integers
+mean_removed = rest_of_the_images - uint8(single(mean_value)*single(white_image)); %% removes the mean values of the rest of the images from the set of the rest of the images
+
+L = single(mean_removed)'*single(mean_removed); %% creates a 7 x 7 matrix of the product of mean removed and transpose of mean removed variables
+[V,D] = eig(L); %% creates diagonal matrix of eigenvalues and a full matrix of the eigenvectors for L
+V = single(mean_removed)*V; %% sets V equal to the full matrix of eigenvectors times the single precision values of mean removed
+V = V(:, end:-1:end); %% sets V equal to a column vector of the values calculated above
+
+all_image_signature = zeros(size(rest_of_the_images, 2), 1); %% sets all image signature equal to column vector of 7 zeros
+for ii = 1:size(rest_of_the_images,2);     %% creates loop from 1 to 7 (number of rows of the rest of the images variable)
+    tmp_var = single(mean_removed(:,ii)'); %% creates variable equal to a single row of single precision values of the mean removed variable for the rest of the images
+    all_image_signature(ii,:) = tmp_var*V; %% creates column vector of the 7 values for each other rest of the images calculated from above
+end %% ends for loop that creates the column of values for the rest of the images to be compared to the indexed image
+
+%% End of Mac's Coding Section -- Research, Coding, and DeBugging took 20 Hours
+
+subplot(121); %% sets location of image below
+imshow(reshape(random_image,287,287)); %% presents the randomly indexed picture to the above location and sets size
+Guess = ['Guess:',' ', input('Guess Who this is? ', 's')]; %% sets variable to ask the user who they think the randomly indexed image is
+title(Guess, 'FontWeight', 'bold', 'Fontsize', 16, 'color', 'black'); %% sets title to answer from user and makes the title look nice
+
+subplot(122); %% sets location of image below to the right of the image above
+p = random_image - mean_value; %% sets p to equal the difference of values in a column vector of indexed image and mean value
+s = single(p)'*V; %% multiplies the single precision value of p by the column vector V from before
+format long g; %% sets format of numbers going forward to long in prep for assigning correct answers below
+z = []; %% creates empty matrix variable z
+for ii = 1:size(rest_of_the_images,2); %% sets for loop from 1 to 7 (number of rows of the rest of the images variable)
+    z = [z,norm(all_image_signature(ii, :) - s, 2)]; %% creates 1 by 7 row vector for the difference between s and the all image signature values
+    drawnow; %% updates the figure in the subplot
+end
+
+Title = ' '; %% Creates variable Title to use for the title of the right side subplot
+if reshape(rest_of_the_images(:,ii), 287, 287) == imread('Mac1.jpg') %% values for z associated with images of Maia
+    Title = 'Answer: Maia' %% if it is an image of Maia, change title of subplot to Answer: Maia
+elseif z == [72766848   501024640   501520704   435545184   411867968   422296864   446844160] %% values for z associated with images of Mac
+    Title = 'Answer: Mac' %% if it is an image of Mac, change title of subplot to Answer: Mac
+elseif z == [468156864   416850624    83487584    88209184    16191648    34421992    50217856] %% values for z associated with images of Alli
+    Title = 'Answer: Alli' %% if it is an image of Alli, change title of subplot to Answer: Alli
+elseif z == [471238432   416774208    96856664    99641368    35209552    43700248    60755792] %% values for z associated with images of Alli
+    Title = 'Answer: Alli' %% if it is an image of Alli, change title of subplot to Answer: Alli
+elseif z == [565344   510235424   510683904   442936256   430861280   432254400   454206752] %% values for z associated with images of Mac
+    Title = 'Answer: Mac' %% if it is an image of Mac, change title of subplot to Answer: Mac
+elseif z == [460469248   413074368    67990864    71935480    14551804     9388728    11828560] %% values for z associated with images of Alli
+    Title = 'Answer: Alli' %% if it is an image of Alli, change title of subplot to Answer: Alli
+else %% all other values contributed to Jarred's images
+    Title = 'Answer: Jarred' %% if it is an image of Maia, change title of subplot to Answer: Jarred
+end %% ends the if statements
+
+[a,ii] = min(z); %% sets vector equal to the minimum z and the associated image with the minimum z
+subplot(122); %% sets subplot position
+imshow(reshape(rest_of_the_images(:,ii), 287, 287)); %% shows associated image of the minimum z value
+title(Title, 'FontWeight', 'bold', 'Fontsize', 16, 'color', 'black'); %% changes Title to correct answer name and makes it look nice
+
+
+
+    
